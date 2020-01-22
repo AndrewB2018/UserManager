40de62d9ad694e91f398b36f61c91f64c720d028
Boynton <andy_boynton@hotmail.co.uk>
2020-01-22 16:22:48 +1100

    Data - Create data repository layer for later use. Added the db context here.

---
+++ UserManager.Data/Context/UserManagerContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using UserManager.DataEntities.Models;
+
+namespace CPUserManager.DAL
+{
+    public class UserManagerContext : DbContext
+    {
+
+        public UserManagerContext() : base("UserManagerContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<UserGroup> UserGroups { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
